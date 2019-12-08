eb2270f9e7b25c0346ea810ef0616d4462d8a28e
eg0000r <47872540+eg0000r@users.noreply.github.com>
2019-12-08 16:29:30 -0500

    Updates

---
+++ precache-manifest.21bab281214f845361f27c8268391358.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0533680564baa69685414bc4bf2f0546",
+    "url": "/stagecar/index.html"
+  },
+  {
+    "revision": "19a606c778b18901da8c",
+    "url": "/stagecar/static/css/main.664da717.chunk.css"
+  },
+  {
+    "revision": "38c738dba7b42f4a5654",
+    "url": "/stagecar/static/js/2.ddb801f3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stagecar/static/js/2.ddb801f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19a606c778b18901da8c",
+    "url": "/stagecar/static/js/main.a2df705f.chunk.js"
+  },
+  {
+    "revision": "36a209caae5abcaefe32",
+    "url": "/stagecar/static/js/runtime-main.9cda6631.js"
+  },
+  {
+    "revision": "054f73c9b5a0e94795cce4a0a5b299d8",
+    "url": "/stagecar/static/media/auto_ru.054f73c9.svg"
+  },
+  {
+    "revision": "add732ed1b05d5a1e3d257375aa142fa",
+    "url": "/stagecar/static/media/logo.add732ed.svg"
+  }
+]);
